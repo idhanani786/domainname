ce032f1c5a4916d80a9da5f502de132282162404
idhanani786 <irshandhanani786@gmail.com>
2022-06-22 00:29:51 +0000

    final

---
+++ app.js
@@ -0,0 +1,12 @@
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+pronoun.forEach((pro)=> {
+    adj.forEach((adj)=> {
+        noun.forEach((no)=>{
+            console.log(pro+adj+no+".com")
+        })
+
+
+    })
+})
